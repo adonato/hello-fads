803b6d2f6ec49be351e8d4dcdd3fdd32ef8fc63d
unknown <adonato@comast.net>
2014-09-16 17:51:06 -0400

    starting over

--- src/com/example/LinkedList.java
+++ src/com/example/LinkedList.java
@@ -8,8 +8,8 @@ public class LinkedList implements Iterable {
     private Node head;
     private Node tail;
     private int size;
-    private String awesomeString;
-     
+    private String someString;
+    
     public Iterator iterator(){
         return new LLIterator();
     }
@@ -118,7 +118,7 @@ public class LinkedList implements Iterable {
     }
     
     public void add(Object obj){
-       Node newNode = new Node(obj, null);
+        Node newNode = new Node(obj, null);
         if( size == 0 )
             head = newNode;
         else
@@ -137,12 +137,12 @@ public class LinkedList implements Iterable {
         }
     }
     
-    public String getAwesomeString() {
-              return awesomeString;
+    public String getSomeString() {
+              return someString;
     }
     
-    public void setAwesomeString(String awesomeString) {
-              this.awesomeString = awesomeString;
+    public void setSomeString(String someString) {
+              this.someString = someString;
     }
     
     public String toString(){
